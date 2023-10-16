f5943e73716c5726ceab7e5e7a2f52f35cd8090b
Emre Kaplan <emrekap@hotmail.com>
2023-10-16 18:26:14 +0300

    sol 34

---
+++ contracts/SafeMathExample34.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.3;
+
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.9/contracts/utils/math/SafeMath.sol";
+
+contract MySafeMathContract34 {
+
+    using SafeMath for uint256;
+    function addition(uint256 avf, uint256 b) public payable returns (bool,uint256) {
+        return avf.tryAdd(b);
+    }
+    
+    function multiply(uint256 a, uint256 b) public payable returns (bool,uint256) { 
+        return SafeMath.tryMul(a,b);
+    }
+       
+}
